95c9b40ec3e207331c6c0904575214b59188d874
一片叶子 <944832260@qq.com>
2020-04-21 11:12:18 +0800

    重复点击路由问题解决

--- src/pages/text/text2.vue
+++ src/pages/text/text2.vue
@@ -1,15 +1,51 @@
 <template>
-    <div>
-        text222222222222222222222222222
-    </div>
+  <div>
+      <p>请求状态：{{content}}</p>
+      <p>count：{{count}}</p>
+      <p v-for="(item,index) in list" :key="index">
+          {{item.info.Content}}
+      </p>
+  </div>
 </template>
 
 <script>
-    export default {
-        name:'Text2'
+import axios from 'axios'
+export default {
+  name: "Text2",
+  data() {
+    return {
+      content: '',
+      count: '',
+      list: [],
+    };
+  },
+  created() {
+    this.getinfo();
+  },
+  methods: {
+    getinfo() {
+      axios
+        .get("/weixin_40755688/phoenix/comment/list/91489538", {
+          params: {
+            page: 1,
+            size: 10,
+            tree_type: 1
+          }
+        })
+        .then(res => {
+          console.log(res, "6666");
+          if (res.content == 'success') {
+              this.content = res.content;
+              this.count = res.data.count;
+              this.list = res.data.list;
+
+          }
+          
+        });
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -15,10 +15,10 @@ import Text3 from '@pages/text/text3.vue'
 
 import VueRouter from 'vue-router'      // 引入路由
 Vue.use(VueRouter)      // 使用中间件使用路由
-// Vue.use(Router)
-
-// const router = 
-
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 export default new VueRouter({
   mode:'history',
   base:__dirname,
